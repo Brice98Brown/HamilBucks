2e5f88b12166cfc5a3592721aca9b3063d476a7f
Brice <brice98brown@gmail.com>
2023-08-05 11:38:03 -0400

    Added some Models

---
+++ HamilServer/Models/ReturnUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamilBucks.HamilServer.Models
+{
+    public class ReturnUser
+    {
+    }
+}
