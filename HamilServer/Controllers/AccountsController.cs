65d3c02abcf92e2f5f7eafce05a62180de0ab916
Brice <brice98brown@gmail.com>
2023-08-07 16:18:33 -0400

    added some packages

--- HamilServer/Controllers/AccountsController.cs
+++ HamilServer/Controllers/AccountsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HamilBucks.HamilServer.Controllers
 {
--- HamilServer/DAO/AcountDAO.cs
+++ HamilServer/DAO/AcountDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace HamilBucks.HamilServer.DAO
 {
     public class AcountDAO
