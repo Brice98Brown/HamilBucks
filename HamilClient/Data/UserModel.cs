0beb85508b3cb254b9af0f12cb5065e2e25e4022
Brice <brice98brown@gmail.com>
2023-08-08 16:52:57 -0400

    added data folder and classes

---
+++ HamilClient/Data/UserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamilBucks.HamilClient.Data
+{
+    internal class UserModel
+    {
+    }
+}
