f4f299de203f62157ff459fffdec904e2bf59e28
Brice <brice98brown@gmail.com>
2023-08-17 18:31:33 -0400

    added stuff to IAccountDAO

--- HamilServer/DAO/IAccountDAO.cs
+++ HamilServer/DAO/IAccountDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HamilBucks.HamilServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace HamilBucks.HamilServer.DAO
 {
-    public class IAccountDAO
+    public interface IAccountDAO
     {
+        Account GetAccountByUserId(int userId);
+        Account GetAccountByAccountId(int accountId);
     }
 }
