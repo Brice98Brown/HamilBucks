f2feea3d450cfdb616c44a4d763fe6fa773b2110
Brice <brice98brown@gmail.com>
2023-08-18 16:18:29 -0400

    added stuff to ITransferDAO

--- HamilServer/DAO/ITransferDAO.cs
+++ HamilServer/DAO/ITransferDAO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HamilBucks.HamilServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace HamilBucks.HamilServer.DAO
 {
-    public class ITransferDAO
+    public interface ITransferDAO
     {
+        Transfers TransferMoneyToUser(int accountIdTo, decimal ammountToTransfer, int accountIdFrom);
+        Transfers RequestTransferMoneyFromUser(int RecepiantAccountId, decimal ammountToTransfer, int SenderAccountId);
+        Transfers UpdateTrasferStatus(int RecepiantAccountId, decimal ammountToTransfer, int transferStatusId, int SenderAccountId, int transferId);
+        List<Transfers> GetAllTransfersForOneUser(int userId);
+        Transfers GetTransferById(int transferId);
     }
 }
