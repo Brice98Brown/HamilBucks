368db99e8b1f978daae8be8dc510991bd501210d
Brice <brice98brown@gmail.com>
2023-08-21 21:56:00 -0400

    added stuff to UserDAO

--- HamilServer/DAO/UserDAO.cs
+++ HamilServer/DAO/UserDAO.cs
@@ -1,12 +1,97 @@
-﻿using System;
+﻿using HamilBucks.HamilServer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HamilBucks.HamilServer.DAO
 {
-    public class UserDAO
+    public class UserDAO : IUserDAO
     {
+        private readonly string connectionString;
+
+        public UserDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public User GetUser(string username)
+        {
+            User returnUser = null;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT user_id, username FROM users WHERE username = @username", conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows && reader.Read())
+                {
+                    returnUser = GetUserFromReader(reader);
+                }
+            }
+
+            return returnUser;
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> returnUsers = new List<User>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT user_id, username FROM users", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        User u = GetUserFromReader(reader);
+                        returnUsers.Add(u);
+                    }
+                }
+            }
+
+            return returnUsers;
+        }
+
+        public User AddUser(string username, string password)
+        {
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO users (username) VALUES (@username)", conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand("SELECT @@IDENTITY", conn);
+                int userId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                cmd = new SqlCommand("INSERT INTO accounts (user_id) VALUES (@userid)", conn);
+                cmd.Parameters.AddWithValue("@userid", userId);
+                cmd.ExecuteNonQuery();
+            }
+
+            return GetUser(username);
+        }
+
+        private User GetUserFromReader(SqlDataReader reader)
+        {
+            return new User()
+            {
+                UserId = Convert.ToInt32(reader["user_id"]),
+                Username = Convert.ToString(reader["username"]),
+               
+            };
+        }
     }
 }
