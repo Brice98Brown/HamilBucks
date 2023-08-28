cfecd730ffa5404b192169d39a156223401825a4
Brice <brice98brown@gmail.com>
2023-08-28 19:30:28 -0400

    added a constructor to accounts controller

--- HamilServer/Controllers/AccountsController.cs
+++ HamilServer/Controllers/AccountsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HamilBucks.HamilServer.DAO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,8 +11,14 @@ namespace HamilBucks.HamilServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountsController
+    public class AccountsController : ControllerBase
     {
-
+        private readonly IAccountDAO accountDAO;
+        private readonly IUserDAO userDAO;
+        public AccountsController(IAccountDAO account, IUserDAO user)
+        {
+            this.accountDAO = account;
+            this.userDAO = user;
+        }
     }
 }
