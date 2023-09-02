9501540a5ad0c0fcbeb86b5352f444735c3f00b4
Brice <brice98brown@gmail.com>
2023-09-01 21:58:39 -0400

    added a constructor to login controller

--- HamilServer/Controllers/AccountsController.cs
+++ HamilServer/Controllers/AccountsController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using HamilBucks.HamilServer.DAO;
+using HamilBucks.HamilServer.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +22,44 @@ namespace HamilBucks.HamilServer.Controllers
         {
             this.accountDAO = account;
             this.userDAO = user;
+        }
+        private int LoggedInUserId
+        {
+            get
+            {
+                Claim idClaim = User.FindFirst("sub");
+                if (idClaim == null)
+                {
+                    // User is not logged in
+                    return -1;
+                }
+                else
+                {
+                    
+                    return int.Parse(idClaim.Value);
+                }
+            }
+        }
+        [HttpGet()]
+        [Authorize]
+        public ActionResult GetAccount()
+        {
+            int userId = LoggedInUserId;
+            if (userId <= 0)
+            {
+                return Unauthorized();
+            }
+            Account account = accountDAO.GetAccountByUserId(userId);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            if (userId != account.UserId)
+            {
+                return Forbid();
+            }
+            return Ok(account);
         }
     }
 }
